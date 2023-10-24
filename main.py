33fa0c191749b656fb7d06b044cb05317ac0df3c
Helen <gubinaelenaaleksandrovna@gmail.com>
2023-10-24 22:13:53 +0300

    Homework 'Поле чудес'  correction 1

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ mask_word = ""
 for i in range(0, len(hidden_word)):
     mask_word += "*"
 print("There is a hidden word:   "+mask_word)
-
+success = False
 
 def check_attempt(inputstr):
+    global success
+    success = False
     mask2 = ""
     if len(inputstr) > 1 and hidden_word == inputstr:
         return hidden_word
@@ -20,15 +22,16 @@ def check_attempt(inputstr):
         for k in range(0, len(hidden_word)):
             if inputstr == hidden_word[k]:
                 mask2 += inputstr
+                success = True
             else:
                 mask2 += mask_word[k]
         return mask2
 
 
 nbr_att = int(input("Input your number of attempts:"))
-
-for i in range(0, nbr_att):
-    str_att = str(input("Input your letter or word:")).lower()
+i = 1
+while i <= nbr_att:
+    str_att = str(input(str(i)+ " Input your letter or word:")).lower()
     if len(str_att) == 0:
         print("You need to enter a letter or word")
     mask_word = check_attempt(str_att)
@@ -37,6 +40,8 @@ for i in range(0, nbr_att):
         break
     else:
         print("Check the word:  " + mask_word)
+    if not success:
+        i += 1
 
 if mask_word != hidden_word:
     print("Game over")
